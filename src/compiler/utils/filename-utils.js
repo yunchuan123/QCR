78dc77f53ffd1439c8a2a90072bf0d8955811057
littleTime <8185129+three-gold-bows@user.noreply.gitee.com>
2023-08-07 17:25:04 +0800

    修复path的bug

--- src/compiler/utils/filename-utils.js
+++ src/compiler/utils/filename-utils.js
@@ -1,3 +1,4 @@
+import { basename } from "path";
 /**
  *
  * @param {string} str
@@ -7,14 +8,16 @@ export function kebabToPascalCase(str) {
     return str.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase());
 }
 
-const filenameRegex = /\/([^/]+)\.vue$/;
-
 /**
  *
  * @param {string} path
  * @returns {*}
  */
 export function getNameByPath(path) {
-    const match = path.match(filenameRegex);
-    return match[1];
+    const name = basename(path)
+    if (!name) {
+        throw new Error("未能匹配到文件名")
+    }
+    const match = name.split(".");
+    return match[0];
 }
