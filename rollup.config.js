a152ab026886478db996323175533ef4c1ae6b17
littleTime <8185129+three-gold-bows@user.noreply.gitee.com>
2023-08-07 16:46:44 +0800

    增加compiler的打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 export default [
     {
         input: "src/compiler/index.js",
         output: {
             file: "dist/compiler.js",
             format: "esm"
-        }
+        },
+        plugins: [
+            json(),
+            commonjs(),
+            resolve({
+                exclude: ['source-map-js', 'css-tree'],
+            })
+        ]
     },
     {
         input: "src/runtime/index.js",
