6326110d4c6616e5d75b5a33df1129694beb6f16
littleTime <8185129+three-gold-bows@user.noreply.gitee.com>
2023-08-09 14:17:55 +0800

    export 更多vue/reactivity的包

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,5 +1,5 @@
 export { createDom, createEffectDom, renderList, useRefs } from "./core/dom/index.js";
-export { reactive } from "@vue/reactivity";
+export { reactive, ref, computed } from "@vue/reactivity";
 export { toDisplayString } from "./utils/index.js";
 export { _carRender, onMounted, useInstance, CustomElement } from "./core/index.js";
 
