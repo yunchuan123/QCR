c36caefe0ec3060b3b19df73993fdf41aeb83d22
littleTime <8185129+three-gold-bows@user.noreply.gitee.com>
2023-09-12 14:35:16 +0800

    增加注释

--- src/runtime/core/dom/attribute/reactive-attribute.js
+++ src/runtime/core/dom/attribute/reactive-attribute.js
@@ -2,13 +2,17 @@ import { effect } from "@vue/reactivity";
 import { updateProp } from "../../props/index.js";
 
 /**
- *
+ * 
  * @param {HTMLElement} el
  * @param {string} name
  * @param {function} fn
  */
 export function createReactiveAttribute(el, name, fn) {
     if (el.localName.includes("-")) {
+        /**
+         * 该段逻辑处理的是props 
+         */
+
         // 使用异步，避免setProps还未挂载到元素身上
         Promise.resolve().then(() => {
             effect(() => {
